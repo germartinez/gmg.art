c090b4ae704c54ab6ede4e477fc583538f1a7e71
Germán Martínez <germaga92@gmail.com>
2021-03-21 20:28:27 +0100

    Add assets configuration

---
+++ src/config/opensea_assets.js
@@ -0,0 +1,33 @@
+const openseaAssets = {
+  "opensea_profile_url": process.env.REACT_APP_OPENSEA_PROFILE_URL,
+  "opensea_collections": [
+    {
+      "collection_url": process.env.REACT_APP_OPENSEA_COLLECTION1_URL,
+      "collection_name": process.env.REACT_APP_OPENSEA_COLLECTION1_NAME,
+      "collection_nfts": [
+        {
+          "nft_image": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT1_IMAGE,
+          "nft_name": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT1_NAME,
+          "nft_url": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT1_URL
+        },
+        {
+          "nft_image": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT2_IMAGE,
+          "nft_name": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT2_NAME,
+          "nft_url": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT2_URL
+        },
+        {
+          "nft_image": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT3_IMAGE,
+          "nft_name": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT3_NAME,
+          "nft_url": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT3_URL
+        },
+        {
+          "nft_image": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT4_IMAGE,
+          "nft_name": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT4_NAME,
+          "nft_url": process.env.REACT_APP_OPENSEA_COLLECTION1_NFT4_URL
+        }
+      ],
+    }
+  ]
+}
+
+export default openseaAssets
